036da169b804a5ab60460944b743f0b71ebde0e0
Daniel Khromov <danielkhromov@gmail.com>
2025-05-14 21:40:32 +0300

    add pulling ollama models before run

--- textgrad-redteam/main.py
+++ textgrad-redteam/main.py
@@ -7,14 +7,21 @@ from textgrad.autograd.string_based_ops import StringBasedFunction
 from textgrad.engine_experimental.litellm import LiteLLMEngine
 import wandb
 from helper_functions import *
-
+import ollama
 
 wandb.login()
 set_seed(12)
 
 os.environ["OLLAMA_BASE_URL"] = config.OLLAMA_BASE_URL
-llm_api_eval = tg.get_engine(config.BACKWARD_MODEL)
+ollama_model_name = "-".join(config.BACKWARD_MODEL.split("-")[1:])
+print(f"Pulling {ollama_model_name}...")
+ollama.pull(ollama_model_name)
+llm_api_eval = tg.get_engine(ollama_model_name)
+
 if "ollama" in config.BASE_MODEL:
+    ollama_model_name = "-".join(config.BASE_MODEL.split("-")[1:])
+    print(f"Pulling {ollama_model_name}...")
+    ollama.pull(ollama_model_name)
     llm_api_test = tg.get_engine(config.BASE_MODEL)
 else:
     llm_api_test = LiteLLMEngine(config.BASE_MODEL)
